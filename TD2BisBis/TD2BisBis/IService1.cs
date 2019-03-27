552b6cd1fdaacc51ea35aa991149ee52c862d77c
Noé Menard <noe.menard@etu.univ-cotedazur.fr>
2019-03-27 10:58:58 +0100

    Added TD2 Rest

---
+++ TD2BisBis/TD2BisBis/IService1.cs
@@ -0,0 +1,10 @@
+﻿using System.ServiceModel;
+namespace WcfJsonRestService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        Person GetData(string id);
+    }
+}
