5f3a88631bbbbaba47c0deec0ccbd0b7fc1356fc
Clement-Durand <clement.durand@etu.unice.fr>
2019-03-27 10:58:06 +0100

    Added REST and SOAP projects

---
+++ MathsLibrary/Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using Client.MathsService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathsOperationsClient client = new MathsOperationsClient("Mathieu");
+            Console.WriteLine(client.Add(100, 101));
+            Console.WriteLine(client.Multiply(100, 101));
+            Console.ReadLine();
+        }
+    }
+}
